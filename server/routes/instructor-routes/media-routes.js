73b49d2d52b3fbad03a006ee8fae471170aaaa52
Saksham Agarwal <114224728+sakshamagarwalm2@users.noreply.github.com>
2025-03-23 14:54:26 +0530

    s

--- server/routes/instructor-routes/media-routes.js
+++ server/routes/instructor-routes/media-routes.js
@@ -7,7 +7,7 @@ const {
 
 const router = express.Router();
 
-const upload = multer({ dest: "uploads/" });
+const upload = multer({ dest: "/tmp/uploads/" });
 
 router.post("/upload", upload.single("file"), async (req, res) => {
   try {
